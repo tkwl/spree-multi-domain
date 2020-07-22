d1741aff786ff6ad3a168e0c9d80d20a9a6025bc
Damian Legawiec <damian@sparksolutions.co>
2020-07-22 12:57:57 +0200

    WIP

--- app/models/spree/store_decorator.rb
+++ app/models/spree/store_decorator.rb
@@ -4,9 +4,6 @@ module Spree
       base.has_and_belongs_to_many :products, join_table: 'spree_products_stores'
       base.has_many :taxonomies
       base.has_and_belongs_to_many :promotion_rules, class_name: 'Spree::Promotion::Rules::Store', join_table: 'spree_promotion_rules_stores', association_foreign_key: 'promotion_rule_id'
-
-      base.has_one_attached :logo
-      base.validates :logo, content_type: ['image/jpeg', 'image/jpg', 'image/png']
     end
   end
 end
