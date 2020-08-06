26c5a48f613634ea03243a48a2155280f04c8a68
Robert Nowakowski <aplegatt@gmail.com>
2020-08-06 12:51:38 +0200

    Adds default layout rendering if store layout is missing

--- lib/spree_multi_domain/engine.rb
+++ lib/spree_multi_domain/engine.rb
@@ -4,6 +4,9 @@ module SpreeMultiDomain
 
     config.autoload_paths += %W[#{config.root}/lib]
 
+    DEFAULT_LAYOUT = 'spree/layouts/spree_application'.freeze
+    @current_store_code = nil
+
     def self.activate
       %w[app lib].each do |dir|
         Dir.glob(File.join(File.dirname(__FILE__), "../../#{dir}/**/*_decorator*.rb")).sort.each do |c|
@@ -20,7 +23,36 @@ module SpreeMultiDomain
     initializer 'templates with dynamic layouts' do
       ActionView::TemplateRenderer.prepend(
         Module.new do
+          def resolve_layout(layout, keys, formats)
+            details = @details.dup
+            details[:formats] = formats
+
+            case layout
+            when String
+              begin
+                if layout.start_with?('/')
+                  ActiveSupport::Deprecation.warn 'Rendering layouts from an absolute path is deprecated.'
+                  @lookup_context.with_fallbacks.find_template(layout, nil, false, [], details)
+                else
+                  @lookup_context.find_template(layout, nil, false, [], details)
+                end
+              rescue ActionView::MissingTemplate
+                if @current_store_code
+                  layout.slice!("/#{@current_store_code}")
+                  @lookup_context.with_fallbacks.find_template(layout, nil, false, [], details)
+                else
+                  @lookup_context.with_fallbacks.find_template(DEFAULT_LAYOUT, nil, false, [], details)
+                end
+              end
+            when Proc
+              resolve_layout(layout.call(@lookup_context, formats), keys, formats)
+            else
+              layout
+            end
+          end
+
           def render_template(view, template, layout_name, locals)
+            @current_store_code = view.current_store.code
             store_layout = if layout_name.is_a?(String)
                              layout_name.gsub('layouts/', "layouts/#{view.current_store.code}/")
                            else
