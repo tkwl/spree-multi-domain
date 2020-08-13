8c96d01d6ec6d06723ddc5d0ee724978772a49f2
Robert Nowakowski <aplegatt@gmail.com>
2020-08-13 16:33:11 +0200

    Adding default store to product stores in migration

--- db/migrate/20100227180338_create_products_stores.rb
+++ db/migrate/20100227180338_create_products_stores.rb
@@ -5,6 +5,12 @@ class CreateProductsStores < SpreeExtension::Migration[4.2]
       t.references :store
       t.timestamps null: false
     end
+
+    default_store_id = Spree::Store.default.id
+    prepared_values = Spree::Product.with_deleted.order(:id).ids.map { |id| "(#{id}, #{default_store_id})" }.join(', ')
+    return if prepared_values.empty?
+
+    execute "INSERT INTO spree_products_stores (product_id, store_id) VALUES #{prepared_values};"
   end
 
   def self.down
