2807e8eeb93709af12e7e0b970fc4d1ded9a0d33
Damian Legawiec <damian@sparksolutions.co>
2020-09-01 18:04:17 +0200

    Expire product cache when assigning/unassigning store

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,7 +1,9 @@
 module Spree
   module ProductDecorator
     def self.prepended(base)
-      base.has_and_belongs_to_many :stores, join_table: 'spree_products_stores'
+      base.has_many :store_products, class_name: 'Spree::StoreProduct', dependent: :destroy
+      base.has_many :stores, through: :store_products, class_name: 'Spree::Store'
+
       base.scope :by_store, ->(store) { joins(:stores).where(spree_products_stores: { store_id: store.id }) }
     end
   end
--- app/models/spree/store_decorator.rb
+++ app/models/spree/store_decorator.rb
@@ -1,7 +1,8 @@
 module Spree
   module StoreDecorator
     def self.prepended(base)
-      base.has_and_belongs_to_many :products, join_table: 'spree_products_stores'
+      base.has_many :store_products, class_name: 'Spree::StoreProduct', dependent: :destroy
+      base.has_many :products, through: :store_products, class_name: 'Spree::Product'
       base.has_many :taxonomies
       base.has_and_belongs_to_many :promotion_rules, class_name: 'Spree::Promotion::Rules::Store', join_table: 'spree_promotion_rules_stores', association_foreign_key: 'promotion_rule_id'
     end
